4363c0ba0fdca5b8b4939e1a7ff3b05ce17c4d7b
채영민\codud <codudals98@gmail.com>
2026-08-13 17:13:21 +0900

    docs: van simulator 타입 역할 주석 추가

--- van-simulator/src/main/java/com/chaeyeongmin/van_sim/transaction/approval/service/support/impl/UuidApprovalNumberGenerator.java
+++ van-simulator/src/main/java/com/chaeyeongmin/van_sim/transaction/approval/service/support/impl/UuidApprovalNumberGenerator.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+/**
+ * UUID 값을 기반으로 VAN 승인번호를 생성하는 기본 구현체다.
+ * <p>
+ * 승인번호는 하이픈을 제거한 UUID 앞 12자리를 대문자로 사용한다.
+ */
 @Component
 public class UuidApprovalNumberGenerator implements ApprovalNumberGenerator {
 
--- van-simulator/src/main/java/com/chaeyeongmin/van_sim/transaction/approval/service/support/impl/UuidVanTransactionIdGenerator.java
+++ van-simulator/src/main/java/com/chaeyeongmin/van_sim/transaction/approval/service/support/impl/UuidVanTransactionIdGenerator.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+/**
+ * UUID 값을 기반으로 VAN 내부 거래번호를 생성하는 기본 구현체다.
+ * <p>
+ * 생성된 식별자는 VAN 거래임을 구분할 수 있도록 {@code VAN-} 접두사를 붙인다.
+ */
 @Component
 public class UuidVanTransactionIdGenerator implements VanTransactionIdGenerator {
 
